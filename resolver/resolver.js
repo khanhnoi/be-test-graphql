9949bd97e0c5af517d0dedc31c491dad4b31a891
khanhnoi <nqkhanh1998@gmail.com>
2021-03-04 21:07:23 +0700

    add mutation

--- resolver/resolver.js
+++ resolver/resolver.js
@@ -1,34 +1,26 @@
+const { books, authors } = require("../data/static");
+
 const resolvers = {
   Query: {
-    books: () => [
-      {
-        id: 1,
-        name: "Dế Mèn Phiêu Kí",
-        genre: "Phiêu Lưu",
-      },
-      {
-        id: 2,
-        name: "Làm giàu không khó",
-        genre: "Giáo Dục",
-      },
-    ],
-    authors: () => [
-      {
-        id: 1,
-        name: "Ngô Tất Tố",
-        age: 127,
-      },
-      {
-        id: 1,
-        name: "Ngô Tất Tố",
-        age: 127,
-      },
-      {
-        id: 1,
-        name: "Ngô Tất Tố",
-        age: 127,
-      },
-    ],
+    books: () => books,
+    book: (parent, args) => books.find((book) => String(book.id) === args.id),
+    authors: () => authors,
+    author: (parent, args) =>
+      authors.find((author) => String(author.id) === args.id),
+  },
+  Book: {
+    author: (parent, args) =>
+      authors.find((author) => String(author.id) == String(parent.authorId)),
+  },
+  Author: {
+    books: (parent, args) =>
+      books.filter((book) => String(book.authorId) === String(parent.id)),
+  },
+
+  //MUTATION
+  Mutation: {
+    createAuthor: (parent, args) => args,
+    createBook: (parent, args) => args,
   },
 };
 
